24d316a8738a5b8825e940d9d0981a87d6d96c32
omarwin21 <omar.zambrano@udea.edu.co>
2022-10-19 13:34:27 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Cards from "./components/cards/cards.js";
+// import Cards from "./components/cards/cards.js";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [poke , setPoke] = useState([])
+
+  async function getPoke() {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1/');
+      // console.log(response.data.name);
+      setPoke (response.data.sprites.front_default)
+      console.log(poke)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(()=>{getPoke()}, []);
+
+
   return (
-    <div>Hola mundo
-      <Cards/>
+    <div>
+      <div>
+        <img src={poke} alt='bu'/>
+      </div>
     </div>
-    
+
   );
 }
 
