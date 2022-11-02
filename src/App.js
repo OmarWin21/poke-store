c98ac93e3eccbfe6f8e4af9e818b62ad16a31ed0
omarwin21 <omar.zambrano@udea.edu.co>
2022-11-02 00:22:30 -0500

    get

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [poke , setPoke] = useState([])
-
+  
   async function getPoke() {
     try {
-      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1/');
+      let arrPoke =[];
+      for(let i = 1; i<=20; i++){
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}/`);
       // console.log(response.data.name);
-      setPoke (response.data.sprites.front_default)
-      console.log(poke)
+      arrPoke.push(response.data.sprites.other["official-artwork"].front_default)
+      console.log(setPoke)
+    }
+    
+    console.log(arrPoke);
+    setPoke (arrPoke)
     } catch (error) {
       console.error(error);
     }
   }
   useEffect(()=>{getPoke()}, []);
-
-
+  
   return (
     <div>
       <div>
-        <img src={poke} alt='bu'/>
+        {/* <img src={poke} alt='bu'/> */}
       </div>
     </div>
 
