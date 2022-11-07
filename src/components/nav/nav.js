930f56e1f0a8b5a5c8f2a6135e2397727a5b091c
omarwin21 <omar.zambrano@udea.edu.co>
2022-11-06 22:44:09 -0500

    adding logo

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../img/pokeLogo.png'
 
 export default function Nav() {
   return (
     <div>
         <nav style= {{backgroundColor : 'red'}}>
             <div className='left-side'>
-                <img src='../img/pokeLogo.png '/>
+                <img src={logo} alt='pokelogo'/>
             </div>
             <div className='right-side'>
                 <ul>
