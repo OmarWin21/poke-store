533e79d7d7c034ea6bac34e5045f93cbee400d10
Mauricio Vélez <serketh7@gmail.com>
2022-11-08 11:18:05 -0500

    ruteo, landing

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <div>
+        <div className='hero'>
+
+            <h2>Bienvenidos</h2>
+        </div>
+    </div>
+  )
+}
